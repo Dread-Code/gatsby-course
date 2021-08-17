c3526131488fdd42e925623cc84eea03d955e340
DreadfulCrazy <lucasospina97@gmail.com>
2021-08-16 22:10:01 -0500

    First steps with react in gatsby

---
+++ src/components/Jumbo/Jumbo.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { StyledJumbo } from "../../styles/components"
+
+const Jumbo = () => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>¡Get the best and exclusive swag of platzi</h2>
+        <small>Elegant clothes for our best students</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
